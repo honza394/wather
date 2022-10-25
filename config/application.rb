9dc5ba7ac8baf83b6d835cbd29d525a1ccd00740
Jan Vacek <honza394@gmail.com>
2022-10-25 11:13:30 +0200

    try deploy 1025 1113

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Wather
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
